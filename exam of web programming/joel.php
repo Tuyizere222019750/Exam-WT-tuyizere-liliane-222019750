62911203f2438618473cbbd7c8dd297dac2ce44f
Tuyizere222019750 <rasaurhuyerwanda@gmail.com>
2024-05-23 13:16:35 +0200

    Add files via upload

---
+++ exam of web programming/joel.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1> welcome php</h1>
+  
+    <?php
+    $num1=20;
+    $num2=10;
+    $sum=$num1+$num2;
+    echo"the sum of number one and number two is:$sum";
+    ?>
+    
+</body>
+</html>
